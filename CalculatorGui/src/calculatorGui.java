12a0092581ddf88a6e1206bf28a985d0e5bbce8c
albertzarifa <albertzarifa@alberts-mbp>
2020-03-08 16:30:26 -0400

    Labels commit

--- CalculatorGui/src/calculatorGui.java
+++ CalculatorGui/src/calculatorGui.java
@@ -7,10 +7,16 @@ import javax.swing.JEditorPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import java.awt.Font;
 
 public class calculatorGui {
 
 	private JFrame frame;
+	private JTextField entered_value;
+	private JTextField stored_value;
 
 	/**
 	 * Launch the application.
@@ -44,72 +50,92 @@ public class calculatorGui {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JEditorPane editorPane = new JEditorPane();
-		editorPane.setBounds(6, 6, 322, 63);
-		frame.getContentPane().add(editorPane);
+		stored_value = new JTextField();
+		stored_value.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		stored_value.setBorder(null);
+		stored_value.setBounds(100, 16, 217, 26);
+		frame.getContentPane().add(stored_value);
+		stored_value.setColumns(10);
 		
-		JButton btnX = new JButton("x");
-		btnX.setBounds(259, 204, 69, 42);
-		frame.getContentPane().add(btnX);
+		entered_value = new JTextField();
+		entered_value.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		entered_value.setBorder(null);
+		entered_value.setBounds(17, 40, 300, 41);
+		frame.getContentPane().add(entered_value);
+		entered_value.setColumns(10);
 		
-		JButton button_8 = new JButton("÷");
-		button_8.setBounds(259, 258, 69, 42);
-		frame.getContentPane().add(button_8);
+		JEditorPane display = new JEditorPane();
+		display.setEnabled(false);
+		display.setBorder(new LineBorder(new Color(0, 0, 0)));
+		display.setBounds(6, 6, 322, 78);
+		frame.getContentPane().add(display);
 		
-		JButton button_9 = new JButton("+");
-		button_9.setBounds(259, 96, 69, 42);
-		frame.getContentPane().add(button_9);
+		JButton btn_zero = new JButton("0");
+		btn_zero.setBounds(6, 258, 45, 42);
+		frame.getContentPane().add(btn_zero);
 		
-		JButton button_10 = new JButton("-");
-		button_10.setBounds(259, 150, 69, 42);
-		frame.getContentPane().add(button_10);
+		JButton btn_one = new JButton("1");
+		btn_one.setBounds(6, 204, 45, 42);
+		frame.getContentPane().add(btn_one);
 		
-		JButton button_11 = new JButton("3");
-		button_11.setBounds(168, 204, 69, 42);
-		frame.getContentPane().add(button_11);
+		JButton btn_two = new JButton("2");
+		btn_two.setBounds(56, 204, 45, 42);
+		frame.getContentPane().add(btn_two);
 		
-		JButton button_12 = new JButton("=");
-		button_12.addActionListener(new ActionListener() {
+		JButton btn_three = new JButton("3");
+		btn_three.setBounds(111, 204, 45, 42);
+		frame.getContentPane().add(btn_three);
+		
+		JButton btn_four = new JButton("4");
+		btn_four.setBounds(6, 150, 45, 42);
+		frame.getContentPane().add(btn_four);
+		
+		JButton btn_five = new JButton("5");
+		btn_five.setBounds(56, 150, 45, 42);
+		frame.getContentPane().add(btn_five);
+		
+		JButton btn_six = new JButton("6");
+		btn_six.setBounds(111, 150, 45, 42);
+		frame.getContentPane().add(btn_six);
+		
+		JButton btn_seven = new JButton("7");
+		btn_seven.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		button_12.setBounds(168, 258, 69, 42);
-		frame.getContentPane().add(button_12);
+		btn_seven.setBounds(6, 96, 45, 42);
+		frame.getContentPane().add(btn_seven);
 		
-		JButton button_13 = new JButton("7");
-		button_13.setBounds(168, 96, 69, 42);
-		frame.getContentPane().add(button_13);
+		JButton btn_eight = new JButton("8");
+		btn_eight.setBounds(56, 96, 45, 42);
+		frame.getContentPane().add(btn_eight);
 		
-		JButton button_14 = new JButton("6");
-		button_14.setBounds(168, 150, 69, 42);
-		frame.getContentPane().add(button_14);
+		JButton btn_nine = new JButton("9");
+		btn_nine.setBounds(111, 96, 45, 42);
+		frame.getContentPane().add(btn_nine);
 		
-		JButton button_3 = new JButton("5");
-		button_3.setBounds(87, 150, 69, 42);
-		frame.getContentPane().add(button_3);
+		JButton btn_equal = new JButton("=");
+		btn_equal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btn_equal.setBounds(56, 258, 100, 42);
+		frame.getContentPane().add(btn_equal);
 		
-		JButton button_4 = new JButton("2");
-		button_4.setBounds(87, 204, 69, 42);
-		frame.getContentPane().add(button_4);
+		JButton btn_add = new JButton("+");
+		btn_add.setBounds(169, 96, 45, 42);
+		frame.getContentPane().add(btn_add);
 		
-		JButton button_6 = new JButton("8");
-		button_6.setBounds(87, 96, 69, 42);
-		frame.getContentPane().add(button_6);
+		JButton btn_sub = new JButton("-");
+		btn_sub.setBounds(169, 150, 45, 42);
+		frame.getContentPane().add(btn_sub);
 		
-		JButton button = new JButton("1");
-		button.setBounds(6, 204, 69, 42);
-		frame.getContentPane().add(button);
+		JButton btn_mult = new JButton("x");
+		btn_mult.setBounds(169, 204, 45, 42);
+		frame.getContentPane().add(btn_mult);
 		
-		JButton button_1 = new JButton("0");
-		button_1.setBounds(6, 258, 150, 42);
-		frame.getContentPane().add(button_1);
-		
-		JButton button_2 = new JButton("4");
-		button_2.setBounds(6, 150, 69, 42);
-		frame.getContentPane().add(button_2);
-		
-		JButton button_15 = new JButton("9");
-		button_15.setBounds(6, 96, 69, 42);
-		frame.getContentPane().add(button_15);
+		JButton btn_div = new JButton("÷");
+		btn_div.setBounds(169, 258, 45, 42);
+		frame.getContentPane().add(btn_div);
 	}
 }
